073987e53539230641ead74d4d9dfa384f7549a1
Victor Jacobs <victor.jacobs@me.com>
2017-02-11 00:08:05 +0100

    Clean controller test boilerplate

--- src/test/java/me/victorjacobs/transactionstatistics/controller/StatisticsControllerTest.java
+++ src/test/java/me/victorjacobs/transactionstatistics/controller/StatisticsControllerTest.java
@@ -1,48 +1,36 @@
 package me.victorjacobs.transactionstatistics.controller;
 
-import me.victorjacobs.transactionstatistics.Application;
+import me.victorjacobs.transactionstatistics.model.Statistic;
+import me.victorjacobs.transactionstatistics.service.StatisticsStore;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.nio.charset.Charset;
-
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 /**
  * Test the StatisticsController. Only end-to-end test to see whether everything is wired up correctly. Correctness is
  * ensured in the StatisticsStoreTest
  * Created by Victor on 10/02/2017.
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
-@WebAppConfiguration
 public class StatisticsControllerTest {
-    @Autowired
-    private WebApplicationContext webApplicationContext;
     private MockMvc mockMvc;
-    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
+    private StatisticsStore statisticsStore;
 
     @Before
     public void setup() {
-        mockMvc = webAppContextSetup(webApplicationContext).build();
+        statisticsStore = mock(StatisticsStore.class);
+        when(statisticsStore.getStatistic()).thenReturn(new Statistic());
+        mockMvc = standaloneSetup(new StatisticsController(statisticsStore)).build();
     }
 
     @Test
     public void getStatistics() throws Exception {
-        mockMvc.perform(get("/statistics")
-                .contentType(contentType))
+        mockMvc.perform(get("/statistics"))
                 .andExpect(status().isOk());
     }
 }
--- src/test/java/me/victorjacobs/transactionstatistics/controller/TransactionControllerTest.java
+++ src/test/java/me/victorjacobs/transactionstatistics/controller/TransactionControllerTest.java
@@ -1,48 +1,35 @@
 package me.victorjacobs.transactionstatistics.controller;
 
-import me.victorjacobs.transactionstatistics.Application;
+import me.victorjacobs.transactionstatistics.service.StatisticsStore;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.nio.charset.Charset;
-
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 /**
  * Test the TransactionController. Only tests here are end-to-end, actual correctness are tested in the StatisticsStoreTest
  * Created by Victor on 10/02/2017.
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
-@WebAppConfiguration
 public class TransactionControllerTest {
-    @Autowired
-    private WebApplicationContext webApplicationContext;
     private MockMvc mockMvc;
-    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
+    private StatisticsStore statisticsStore;
 
     @Before
     public void setup() {
-        mockMvc = webAppContextSetup(webApplicationContext).build();
+        statisticsStore = mock(StatisticsStore.class);
+        mockMvc = standaloneSetup(new TransactionController(statisticsStore)).build();
     }
 
     @Test
     public void postTransaction() throws Exception {
         mockMvc.perform(post("/transactions")
                 .content("{\"timestamp\": 11111111, \"amount\": 12.1}")
-                .contentType(contentType))
+                .contentType(MediaType.parseMediaType("application/json;charset=UTF-8")))
             .andExpect(status().isCreated());
     }
 
@@ -50,7 +37,7 @@ public class TransactionControllerTest {
     public void postTransaction_invalidJson() throws Exception {
         mockMvc.perform(post("/transactions")
                 .content("foobar")
-                .contentType(contentType))
+                .contentType(MediaType.parseMediaType("application/json;charset=UTF-8")))
                 .andExpect(status().isBadRequest());
     }
 }
